a538a87c3ccbc61ff519f80121a73c8ef37c42dc
Wesley Ng <wesjng+learnco@gmail.com>
2019-02-03 06:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber.constructor !== int) {
+  if (cardNumber.constructor !== Number) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
